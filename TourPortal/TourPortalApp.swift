230ce06444495415d4be385ebb9ff5fa8790cca6
Max <ishashkov.m@gmail.com>
2022-03-26 14:56:26 +0300

    Initial Commit

---
+++ TourPortal/TourPortalApp.swift
@@ -0,0 +1,17 @@
+//
+//  TourPortalApp.swift
+//  TourPortal
+//
+//  Created by Max Shashkov on 26.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct TourPortalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
