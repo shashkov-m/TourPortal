ddffd41d1507735ef5847ddb71a97392c85440ae
Max <ishashkov.m@gmail.com>
2022-03-28 21:53:06 +0300

    1.0

--- TourPortal/Views/NewsfeedViews/NewsfeedView.swift
+++ TourPortal/Views/NewsfeedViews/NewsfeedView.swift
@@ -8,13 +8,22 @@
 import SwiftUI
 
 struct NewsfeedView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+  var body: some View {
+    NavigationView {
+      ScrollView (.vertical, showsIndicators: false) {
+        VStack (spacing: 0) {
+          NewsTileView(image: Image ("2"))
+          NewsTileView(image: Image ("4"))
+          NewsTileView(image: Image ("3"))
+        }
+      }
+      .navigationTitle("Новости")
     }
+  }
 }
 
 struct NewsfeedView_Previews: PreviewProvider {
-    static var previews: some View {
-        NewsfeedView()
-    }
+  static var previews: some View {
+    NewsfeedView()
+  }
 }
